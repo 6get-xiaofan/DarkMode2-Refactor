f1b21565778bfb892a32f0fdd44d478642f2d626
Gabriel Ryder <chinabga@gmail.com>
2025-06-08 20:50:45 +0800

    refactor: Log Module
    Add Factory Model

--- DarkMode.Core/Interfaces/Logging/ILoggerService.cs
+++ DarkMode.Core/Interfaces/Logging/ILoggerService.cs
@@ -1,12 +1,17 @@
+using System.Runtime.CompilerServices;
 using DarkMode.Core.Enums;
 
 namespace DarkMode.Core.Interfaces.Logging;
 
+/**
+ * 日志
+ */
 public interface ILoggerService
 {
     void SetLogLevel(LogLevel level);
-    void Debug(string module, string @namespace, string className, string method, string message);
-    void Info(string module, string @namespace, string className, string method, string message);
-    void Warn(string module, string @namespace, string className, string method, string message);
-    void Error(string module, string @namespace, string className, string method, string message);
+    void Debug(string message, [CallerMemberName] string method = "");
+    void Info(string message, [CallerMemberName] string method = "");
+    void Warn(string message, [CallerMemberName] string method = "");
+    void Error(string message, [CallerMemberName] string method = "");
+    
 }
